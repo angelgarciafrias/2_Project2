bc993e9d12d8e05714377fdbb5c5f8bafb0f7dc6
Angel Garcia Frias <angelgarciafrias@Angels-MBP.home>
2020-06-05 11:53:49 +0200

    Channel working

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SESSION_PERMANET"] = True
@@ -17,17 +17,20 @@ active_users_list = []
 channel_list = []
 chat_list = dict()
 
-@app.route("/register/", methods = ["GET", "POST"])
+
+@app.route("/register/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
       session["username"] = request.form.get("username")
       if session["username"] in user_list:
-          return render_template("error.html",message="Please, try with a different username")
+          return render_template("error.html", message="Please, try with a different username")
+
       user_list.append(session["username"])
       session.permanent = True
       return redirect(url_for('index'))
 
     return render_template("register.html")
+
 
 @app.route("/logout/")
 def logout():
@@ -40,58 +43,70 @@ def logout():
     session.clear()
     return redirect("/")
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     if "username" in session:
         if request.method == "POST":
             new_channel = request.form.get("new-channel")
-            
+
             if new_channel in channel_list:
                 return render_template("error.html", message="Not a valid channel.")
-            
+
             channel_list.append(new_channel)
-            chat_list[new_channel] = [["","system","Welcome to the new channel"]]
+            chat_list[new_channel] = [
+                ["", "system", "Welcome to the new channel"]]
             return redirect("/channel/" + new_channel)
-        
+
         if session["username"] not in active_users_list:
             active_users_list.append(session['username'])
 
-        return render_template("index.html",channel_list=channel_list,active_users_list=active_users_list)
+        return render_template("index.html", channel_list=channel_list, active_users_list=active_users_list)
 
     return render_template("register.html")
+
 
 @app.route("/channel/<string:channel>")
 def channel(channel):
 
     if channel in channel_list:
         session["channel"] = channel
-        return render_template("chat.html",chat_list=chat_list[channel])
+        return render_template("chat.html", chat_list=chat_list[channel])
     else:
         return render_template("error.html", message="Not a valid channel.")
 
+
+@socketio.on("enter")
+def enter_channel():
+
+    channel = session.get("channel")
+    join_room(channel)
+
+    emit("update message", {
+        "timestamp": "--------------", "username": session.get("username"),
+        "message": "Has entered the channel--------------"
+    }, room=channel)
+
+
+@socketio.on("leave")
+def leave_channel():
+
+    channel = session.get("channel")
+    leave_room(channel)
+
+    emit("update message", {
+        "timestamp": "--------------", "username": session.get("username"),
+        "message": "Has left the channel--------------"
+    }, room=channel)
+
 @socketio.on("send message")
 def send_message(timestamp, username, message):
-    
+
     channel = session.get("channel")
     if len(chat_list[channel]) > 100:
         chat_list[channel].pop(0)
     chat_list[channel].append([timestamp, session.get("username"), message])
 
-    emit("update message", {"timestamp": timestamp,"username": session.get("username"), "message": message}, broadcast=True)
-
-@socketio.on("enter")
-def enter_channel():
-
-    emit("update message", {
-        "timestamp": "--------------", "username": session.get("username"),
-        "message": "Has entered the channel--------------"
-        }, broadcast=True)
-
-@socketio.on("leave")
-def leave_channel():
-
-    emit("update message", {
-        "timestamp": "--------------", "username": session.get("username"),
-        "message": "Has left the channel--------------"
-        }, broadcast=True)
+    emit("update message", {"timestamp": timestamp, "username": session.get("username"), "message": message
+                            }, room=channel)
